7995863529646d1b6ef0b02c1565c8c5dad1540f
Tomasz Pasternak <pasternak.t@gmail.com>
2017-06-07 20:11:16 +0200

    Command line working & project info

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
 module Main where
 
-import Lib
+import           Control.Monad
+import           Iec61850.Client
+import           System.Console.CmdArgs
+
+data IedClient = IedClient { address :: String, port :: Integer }
+  deriving (Show, Data, Typeable)
+
+iedclient = IedClient { address = "localhost" &= help "IP Address", port = 102 &= help "IP Port" } &= summary
+                                                                                                        "IEC 61850 device client"
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- cmdArgs iedclient
+  print args
+  con <- connect (address args) (fromInteger . port $ args)
+  model <- discover con
+  forM_ model print
