b535b5834f49b2429d14c54d29b0be070250b83a
Tomasz Pasternak <pasternak.t@gmail.com>
2017-07-17 22:54:42 +0200

    Data reloaded each 5 seconds

--- app/Main.hs
+++ app/Main.hs
@@ -28,7 +28,8 @@ import Brick.Widgets.Edit
 import Brick.Widgets.Core
 import Data.Bits
 import Graphics.Vty
-
+import Control.Concurrent
+import Brick.BChan (newBChan, writeBChan)
 import qualified Brick.Focus as F
 
 data Name = FilterField | ListField | Viewport1 deriving (Eq,Show,Ord)
@@ -70,15 +71,15 @@ data Model = Model {
 makeLenses ''Model
 
 fieldsListV :: Model -> Widget Name
-fieldsListV m = border $ viewport Viewport1 Vertical $ vBox $ (vLimit 1 <$> visibleXs)
+fieldsListV m = border $ viewport Viewport1 Vertical $ vBox (vLimit 1 <$> visibleXs)
   where visibleXs = over (element $ m ^. selection) (visible . withAttr (attrName "blueBg")) stringedXs
-        stringedXs = map (\(x,y,z) -> hLimit 60 (str x <+> fill ' ' )<+> str (show y) <+> str "  "  <+> str(show z)) (m ^. matchingFields)
+        stringedXs = map (\(x,y,z) -> hLimit 50 (str x <+> fill ' ') <+> str (show y) <+> str "  "  <+> str(show z) <+> fill ' ') (m ^. matchingFields)
 
 drawUI :: Model -> [Widget Name]
 drawUI m = [e <=> fieldsListV m]
   where e = vLimit 3 $ border (F.withFocusRing (m^. focusRing) renderEditor (m^. edit1)) 
 
-app :: M.App Model e Name
+app :: M.App Model Tick Name
 app =
     M.App { M.appDraw = drawUI
           , M.appStartEvent = return
@@ -86,6 +87,12 @@ app =
           , M.appAttrMap = const $ attrMap V.defAttr [ (attrName "blueBg", Brick.Util.bg Graphics.Vty.blue)]                            
           , M.appChooseCursor =  F.focusRingCursor (^.focusRing)
           }
+
+mmsReadSeries con model = do
+  sts <- forM model $ \(ref, fc) -> do
+    val <- readVal con ref fc
+    return (ref, fc, val)
+  return sts
 
 main :: IO ()
 main = do
@@ -109,17 +116,20 @@ main = do
                      fetchAndSaveModel con modelsDir modelFile
 
   let modelFiltered = filter (\(ref, _) -> ref =~ filterExp args) model
-  sts <- forM modelFiltered $ \(ref, fc) -> do
-    val <- readVal con ref fc
-    return (ref, fc, val)
+  sts <- mmsReadSeries con modelFiltered
   if tui args then do
-    x <- defaultMain app (initialState sts)
+    chan <- newBChan 10
+    forkIO $ forever $ do
+      sts <- mmsReadSeries con modelFiltered
+      writeBChan chan (Tick sts)
+      threadDelay 5000000
+    x <- customMain (V.mkVty V.defaultConfig) (Just chan) app (initialState sts)
     return ()
   else
     forM_ sts $ \(ref, fc, val) -> putStrLn $ ref ++ "[" ++ show fc ++ "]: " ++ show val
 
-initialState sts =(Model sts sts 0 "" (F.focusRing [FilterField, FilterField])
-       (editor FilterField (str . unlines) Nothing ""))
+initialState sts =Model sts sts 0 "" (F.focusRing [FilterField, FilterField])
+                  (editor FilterField (str . unlines) Nothing "")
 
 moveDown st
   | st ^. selection == length (st ^. matchingFields) -1 = st
@@ -129,18 +139,27 @@ moveUp st
   | st ^. selection == 0 = st
   | otherwise = over selection (\x -> x - 1) st
 
+data Tick = Tick [(String,FunctionalConstraint,MmsVar)]
 
-appEvent :: Model -> T.BrickEvent Name e -> T.EventM Name (T.Next Model)
+updateMatchingXs ss = 
+  let regexString = head $ getEditContents $ ss ^. edit1
+      matchingXs = filter ((=~ regexString) . \(x,_,_) -> x)  (ss ^. fields)
+      ss2 = set matchingFields matchingXs ss
+  in over selection (\x -> max  0 (min x (length matchingXs - 1))) ss2
+
+
+appEvent :: Model -> T.BrickEvent Name Tick -> T.EventM Name (T.Next Model)
 appEvent st (T.VtyEvent (V.EvKey V.KDown [])) = M.continue $ moveDown st
+appEvent st (AppEvent (Tick sts)) = do
+  let ss = set fields sts st
+  let ss2 = updateMatchingXs ss
+  M.continue ss2
 appEvent st (T.VtyEvent (V.EvKey V.KUp [])) = M.continue $ moveUp st
 appEvent st (T.VtyEvent (V.EvKey V.KEsc [])) = M.halt st
 appEvent st (T.VtyEvent (V.EvKey (V.KChar '\t') [])) = M.continue $ st & focusRing %~ F.focusNext
 appEvent st (T.VtyEvent e) = do
   ss <- case F.focusGetCurrent (st ^. focusRing) of
     Just FilterField -> T.handleEventLensed st edit1 handleEditorEvent e
-  let regexString = head $ getEditContents $ ss ^. edit1
-  let matchingXs = filter ((=~ regexString) . \(x,_,_) -> x)  (ss ^. fields)
-  let ss2 = set matchingFields matchingXs ss
-  let ss3 = over selection (\x -> max  0 (min x ((length matchingXs)-1))) ss2
-  continue $ ss3
+  let ss2 = updateMatchingXs ss
+  continue ss2
 
