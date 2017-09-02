e368c22c73b9ab3749ecaf69ce39a24cf2f94f03
Tomasz Pasternak <pasternak.t@gmail.com>
2017-09-02 21:33:40 +0200

    Handling write failures

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,14 @@
 
 module Main where
 
-import           Control.Monad
-import           Iec61850.Client
-import           System.Directory
-import           System.IO
-import           Text.Read
-import           Text.Regex.TDFA
+import Control.Monad
+import Control.Monad.Except(liftIO, runExceptT)
+import Data.Either.Utils(fromRight)
+import Iec61850.Client
+import System.Directory
+import System.IO
+import Text.Read
+import Text.Regex.TDFA
 import Control.Concurrent
 import Brick.BChan (newBChan, writeBChan)
 import Tui(Tick(..), Request(..), tuiMain)
@@ -30,18 +32,18 @@ iedclient =
       { address      = "localhost" &= help "IP Address"
       , port         = 102 &= help "IP Port"
       , refreshCache = False &= help "Refresh cached model for the device"
-      , filterExp    = def &= help "Filter fields by this regex"
+      , filterExp    = ".*" &= help "Filter fields by this regex"
       , tui          = False &= help "Terminal user interface"
       }
     &= summary "IEC 61850 device client"
 
 fetchAndSaveModel con modelsDir modelFile = do
   model_ <- discover con
-  createDirectoryIfMissing True modelsDir
-  (path, file) <- openTempFile modelsDir "temporaryModel"
-  hPutStr file (show model_)
-  hClose file
-  renameFile path modelFile
+  liftIO $ createDirectoryIfMissing True modelsDir
+  (path, file) <- liftIO $ openTempFile modelsDir "temporaryModel"
+  liftIO $ hPutStr file (show model_)
+  liftIO $ hClose file
+  liftIO $ renameFile path modelFile
   return model_
 
 mmsReadSeries con model = forM model $ \(ref, fc) -> do
@@ -51,13 +53,13 @@ mmsReadSeries con model = forM model $ \(ref, fc) -> do
 main :: IO ()
 main = do
   ic    <- cmdArgs iedclient
-  con     <- connect (address ic) (fromInteger . port $ ic)
+  con     <- fromRight <$> runExceptT (connect (address ic) (fromInteger . port $ ic))
   homeDir <- getHomeDirectory
   let modelsDir = homeDir ++ "/" ++ ".iedclient.d/models/"
   let modelFile = modelsDir ++ "/" ++ address ic
   modelFileExists <- doesPathExist modelFile
   model       <- if not modelFileExists || refreshCache ic
-    then fetchAndSaveModel con modelsDir modelFile
+    then fromRight <$> runExceptT (fetchAndSaveModel con modelsDir modelFile)
     else do
       contents <- withFile modelFile ReadMode $
         \h -> do
@@ -65,8 +67,7 @@ main = do
           return $!! contents
       case readMaybe contents of
         Just x -> return x
-        Nothing -> fetchAndSaveModel con modelsDir modelFile
-
+        Nothing -> fromRight <$> runExceptT (fetchAndSaveModel con modelsDir modelFile)
   if tui ic
     then do
       chan <- newBChan 10
@@ -74,20 +75,25 @@ main = do
       _ <- forkIO $ forever $ do
         req <- takeMVar mv
         case req of
-          ReadRequest r -> do
-            let listOfFieldsToUpdate = r
-            sts <- mmsReadSeries con listOfFieldsToUpdate
+          ReadRequest listOfFieldsToUpdate -> do
+            sts <- runExceptT $ mmsReadSeries con listOfFieldsToUpdate
             writeBChan chan $ Read sts
           WriteRequest ref fc val -> do
-            writeVal con ref fc val
-            sts <- mmsReadSeries con [(ref,fc)]
-            writeBChan chan $ Read sts
+            writeRes <- runExceptT $ writeVal con ref fc val
+            case writeRes of 
+              Right _ -> do
+                sts <- runExceptT $ mmsReadSeries con [(ref,fc)]
+                writeBChan chan $ Read sts
+              Left err -> writeBChan chan $ Read (Left err)
       let sts = zip model $ repeat Nothing
       _ <- tuiMain chan sts mv
       return ()
     else do
       let modelFiltered = filter (\(ref, _) -> ref =~ filterExp ic) model
-      sts <- mmsReadSeries con modelFiltered
-      forM_ sts $ \((ref, fc), val) ->
-        putStrLn $ ref ++ "[" ++ show fc ++ "]: " ++ maybe "" show val
+      res <- runExceptT $ mmsReadSeries con modelFiltered
+      case res of
+        Right sts -> 
+          forM_ sts $ \((ref, fc), val) ->
+                        putStrLn $ ref ++ "[" ++ show fc ++ "]: " ++ maybe "" show val
+        Left err -> hPutStrLn stderr err
 
--- src/Tui.hs
+++ src/Tui.hs
@@ -5,6 +5,7 @@
 
 module Tui where
 
+import Data.Either(isRight)
 import Control.Monad
 import Iec61850.Client
 import Iec61850.Mms
@@ -56,18 +57,20 @@ data AppState = AppState {
   _mv :: MVar Request,
   _refreshing :: Bool,
   _start :: Int,
-  _size :: Int
+  _size :: Int,
+  _message :: String
   }
 
 makeLenses ''AppState
 
 fieldsListV :: AppState -> Widget Name
-fieldsListV m = border $ vLimit (m ^. size) $ vBox  (vLimit 1 <$> visibleXs) <=> fill ' '
+fieldsListV m =
+  border $ vLimit (m ^. size) $ vBox (vLimit 1 <$> visibleXs) <=> fill ' '
  where
-  visibleXs = take (m ^. size) $ drop (m ^. start) $ highlightedXs
+  visibleXs     = take (m ^. size) $ drop (m ^. start) $ highlightedXs
   highlightedXs = over (element $ m ^. selection)
-                   (visible . withAttr (attrName "blueBg"))
-                   stringedXs
+                       (visible . withAttr (attrName "blueBg"))
+                       stringedXs
   stringedXs = map
     ( \((x, y), z) ->
       hLimit 50 (str x <+> fill ' ')
@@ -126,27 +129,31 @@ selectionWidget m = vBox
 
 drawUI :: AppState -> [Widget Name]
 drawUI m =
-  [(e <+> selectionW) <=> refreshingStatus <=> fieldsListV m <=> helpBar]
+  [ (e <+> selectionW)
+      <=> refreshingStatus
+      <=> fieldsListV m
+      <=> helpBar
+      <=> messageBar
+  ]
  where
-  e = vLimit 1 $ hLimit 50 $ filterLabel<+> F.withFocusRing
+  e = vLimit 1 $ hLimit 50 $ filterLabel <+> F.withFocusRing
     (m ^. focusRing)
     renderEditor
     (m ^. editFilter)
-  filterLabel = if m^.validFilter then str "Filter: " else withAttr (attrName "redFg") (str "Filter: ")
+  filterLabel = if m ^. validFilter
+    then str "Filter: "
+    else withAttr (attrName "redFg") (str "Filter: ")
   refreshingStatus = str (if m ^. refreshing then "Refreshing" else " ")
-  helpBar =
-    blackOnWhite (str "F5")
-      <+> str " read | "
-      <+> blackOnWhite (str "Esc")
-      <+> str " exit"
-  selectionW = selectionWidget m
+  helpBar          = str "F5" <+> str " read | " <+> str "Esc" <+> str " exit"
+  selectionW       = selectionWidget m
+  messageBar       = vLimit 1 $ str (m ^. message)
 
 app :: M.App AppState Tick Name
 app = M.App
-  { M.appDraw        = drawUI
-  , M.appStartEvent  = return
-  , M.appHandleEvent = appEvent
-  , M.appAttrMap     = const $ attrMap
+  { M.appDraw         = drawUI
+  , M.appStartEvent   = return
+  , M.appHandleEvent  = appEvent
+  , M.appAttrMap      = const $ attrMap
     V.defAttr
     [ (attrName "blueBg" , Brick.Util.bg Graphics.Vty.blue)
     , (attrName "blackFg", Brick.Util.fg Graphics.Vty.black)
@@ -160,45 +167,60 @@ mmsReadSeries con model = forM model $ \(ref, fc) -> do
   val <- readVal con ref fc
   return ((ref, fc), Just val)
 
+initialFilter :: String
+initialFilter = "."
+
 initialState sts mv initSize = AppState
   sts
   (DM.toList sts)
   0
-  ""
+  initialFilter
   (F.focusRing [FilterEditor, ValueEditor])
-  (editor FilterEditor (str . unlines) Nothing "")
-  False
+  (editor FilterEditor (str . unlines) Nothing initialFilter)
+  (isRight $ getRegex initialFilter)
   (editor ValueEditor (str . unlines) Nothing "")
   mv
   False
   0
   initSize
+  "OK"
 
-moveDown st | st ^. selection == length (st ^. matchingFields) - 1 = st
-            | (st ^. selection) - (st ^. start) == (st ^. size) - 1 = over selection (+1) . over start (+1) $ st
-            | otherwise = over selection (+1) st
+moveDown st
+  | st ^. selection == length (st ^. matchingFields) - 1
+  = st
+  | (st ^. selection) - (st ^. start) == (st ^. size) - 1
+  = over selection (+1) . over start (+1) $ st
+  | otherwise
+  = over selection (+1) st
 
-moveUp st | st ^. selection == 0 = st
-          | st ^. selection == st ^. start = over selection (\x -> x - 1) . over start (\x -> x - 1) $ st
-          | otherwise = over selection (\x -> x - 1) st
+moveUp st
+  | st ^. selection == 0
+  = st
+  | st ^. selection == st ^. start
+  = over selection (\x -> x - 1) . over start (\x -> x - 1) $ st
+  | otherwise
+  = over selection (\x -> x - 1) st
 
-newtype Tick = Read [((String,FunctionalConstraint),Maybe MmsVar)]
+newtype Tick = Read (Either String [((String,FunctionalConstraint),Maybe MmsVar)])
 
 getRegex :: String -> Either String Regex
-getRegex regexString = 
+getRegex regexString =
   let compOption = CompOption False False False False False
       execOption = ExecOption False
-  in compile compOption execOption regexString
+  in  compile compOption execOption regexString
 
-getMatchingFields :: Regex -> AppState -> DM.Map (String, FunctionalConstraint) (Maybe MmsVar)
-getMatchingFields regex st = DM.filterWithKey (\(x, _) _ -> matchTest regex x) (st ^. fields)
+getMatchingFields
+  :: Regex -> AppState -> DM.Map (String, FunctionalConstraint) (Maybe MmsVar)
+getMatchingFields regex st =
+  DM.filterWithKey (\(x, _) _ -> matchTest regex x) (st ^. fields)
 
 updateMatchingXs :: AppState -> AppState
-updateMatchingXs ss = 
+updateMatchingXs ss =
   let regex = getRegex $ head $ getEditContents $ ss ^. editFilter
-      ss2 = case regex of
-          Right r -> set validFilter True $ set matchingFields (DM.toList $ getMatchingFields r ss) ss
-          Left l -> set validFilter False ss
+      ss2   = case regex of
+        Right r -> set validFilter True
+          $ set matchingFields (DM.toList $ getMatchingFields r ss) ss
+        Left l -> set validFilter False ss
   in  set selection 0 . set start 0 $ ss2
 
 createMmsVar :: String -> Constr -> Maybe MmsVar
@@ -208,11 +230,11 @@ createMmsVar strVal t = if t == toConstr (MmsInteger 0)
     let newValMms = MmsInteger newValInt
     return newValMms
   else if t == toConstr (MmsFloat 4.0)
-  then do
-    newValDouble <- readMaybe strVal
-    let newValMms = MmsFloat newValDouble
-    return newValMms
-  else Nothing
+    then do
+      newValDouble <- readMaybe strVal
+      let newValMms = MmsFloat newValDouble
+      return newValMms
+    else Nothing
 
 writeRequest st = case selectedType st of
   Just t ->
@@ -227,13 +249,16 @@ appEvent
   :: AppState -> T.BrickEvent Name Tick -> T.EventM Name (T.Next AppState)
 appEvent st (T.VtyEvent (V.EvKey V.KEsc  [])) = M.halt st
 appEvent st (T.VtyEvent (V.EvKey V.KDown [])) = M.continue $ moveDown st
-appEvent st (AppEvent   (Read sts          )) = do
-  let stsMerged = DM.unionWith (flip const) (st ^. fields) (DM.fromList sts)
-  M.continue
-    $ set refreshing False
-    . updateMatchingXs
-    . set fields stsMerged
-    $ st
+appEvent st (AppEvent   (Read received     )) = case received of
+  Right sts -> do
+    let stsMerged = DM.unionWith (flip const) (st ^. fields) (DM.fromList sts)
+    M.continue
+      $ set refreshing False
+      . updateMatchingXs
+      . set fields  stsMerged
+      . set message "OK"
+      $ st
+  Left err -> M.continue $ set message err st
 appEvent st (T.VtyEvent (V.EvKey (V.KFun 5) [])) = if not $ st ^. refreshing
   then M.suspendAndResume $ do
     putMVar (st ^. mv) $ ReadRequest $ map fst (st ^. matchingFields)
@@ -248,8 +273,7 @@ appEvent st (T.VtyEvent (V.EvKey V.KEnter [])) = case writeRequest st of
 appEvent st (T.VtyEvent (V.EvKey (V.KChar '\t') [])) =
   M.continue $ st & focusRing %~ F.focusNext
 appEvent st (T.VtyEvent (V.EvResize x y)) =
-  let newSt = set size (y - 10) st
-  in  M.continue newSt
+  let newSt = set size (y - 10) st in M.continue newSt
 appEvent st (T.VtyEvent e) = do
   newSt <- case F.focusGetCurrent (st ^. focusRing) of
     Just FilterEditor -> do
@@ -260,7 +284,8 @@ appEvent st (T.VtyEvent e) = do
 
 tuiMain chan sts mv = do
   initSize <- Size.size
-  customMain (V.mkVty V.defaultConfig)
+  customMain
+    (V.mkVty V.defaultConfig)
     (Just chan)
     app
     (initialState (DM.fromList sts) mv (Size.height (fromJust initSize) - 10))
